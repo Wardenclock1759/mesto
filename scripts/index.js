f3128bbe76cf98a53aa4cc0e4a034ae39ade2a7f
Misha Kukarkin <wardenclock@gmail.com>
2023-01-30 22:20:23 +0500

    fix: form saved value on close/open

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const likeButtons = document.querySelectorAll(".elements__like-button"); 
 const editButton = document.querySelector(".profile__edit-button"); 
+const closeButton = document.querySelector(".popup__close-button"); 
 
 likeButtons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -11,15 +12,21 @@ let formElement = document.querySelector(".popup");
 let nameText = document.querySelector(".profile__title"); 
 let jobText = document.querySelector(".profile__subtitle"); 
 
+let nameInput = document.querySelector(".popup__title-input"); 
+let jobInput = document.querySelector(".popup__subtitle-input"); 
+
 editButton.addEventListener("click", () => {
+    formElement.classList.toggle("popup_opened")
+    nameInput.value = nameText.textContent;
+    jobInput.value = jobText.textContent;
+});
+
+closeButton.addEventListener("click", () => {
     formElement.classList.toggle("popup_opened")
 });
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
-
-    let nameInput = document.querySelector(".popup__title-input"); 
-    let jobInput = document.querySelector(".popup__subtitle-input"); 
 
     nameText.textContent = nameInput.value;
     jobText.textContent = jobInput.value;
