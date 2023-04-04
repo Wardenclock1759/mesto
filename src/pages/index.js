ef75640485a6b68fed94ee96844220d46de07a5b
Misha Kukarkin <wardenclock@gmail.com>
2023-04-04 21:40:03 +0500

    minor fixes to account for the other changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import "./pages/index.css";
+import "../pages/index.css";
 
-import Section from "./components/Section.js";
-import Card from "./components/Card.js";
-import UserInfo from "./components/UserInfo.js";
-import PopupWithImage from "./components/PopupWithImage.js";
-import PopupWithForm from "./components/PopupWithForm.js";
-import FormValidator from "./components/FormValidator.js";
+import Section from "../components/Section.js";
+import Card from "../components/Card.js";
+import UserInfo from "../components/UserInfo.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import FormValidator from "../components/FormValidator.js";
 
 import {
     cards,
@@ -19,28 +19,28 @@ import {
     addButtonSelector,
     editButtonSelector,
     validationSettings
-} from "./vendor/constants.js";
+} from "../utils/constants.js";
 
 const addButton = document.querySelector(addButtonSelector);
 const editButton = document.querySelector(editButtonSelector);
 const imagePopup = new PopupWithImage(imagePopupSelector);
 const profilePopup = new PopupWithForm(profileFormSelector, handleProfileSubmit);
 const cardPopup = new PopupWithForm(photoFormSelector, handleCardSubmit);
-const forms = Array.from(document.querySelectorAll(validationSettings.formSelector));
+
+const profileValidation = new FormValidator(validationSettings, document.querySelector(profileFormSelector));
+const cardValidation = new FormValidator(validationSettings, document.querySelector(photoFormSelector));
 
 const userInfo = new UserInfo({
     nameSelector: profileNameSelector,
     aboutSelector: profileAboutSelector
 });
 
-const cardSection = new Section({
-    items: cards,
-    renderer: (item) => {
+const cardSection = new Section(
+    (item) => {
         const card = new Card(item, cardTemplateSelector, () => handleCardClick(item));
         const cardElement = card.generateCard();
         cardSection.addItem(cardElement);
-    }
-}, cardListSelector);
+    }, cardListSelector);
 
 function handleCardClick(cardData) {
     imagePopup.open(cardData);
@@ -48,29 +48,34 @@ function handleCardClick(cardData) {
 
 function handleProfileSubmit(user) {
     userInfo.setUserInfo(user);
+    this.close();
 }
 
 function handleCardSubmit(cardData) {
     const card = new Card(cardData, cardTemplateSelector, () => handleCardClick(cardData));
     const cardElement = card.generateCard();
     cardSection.addItem(cardElement);
+    this.close();
+}
+
+function handleEditButtonClick() {
+    profileValidation.resetErrors();
+    profilePopup.setInputValues(userInfo.getUserInfo());
+    profilePopup.open();
+}
+
+function handleAddButtonClick() {
+    cardValidation.resetErrors();
+    cardPopup.open();
 }
 
 imagePopup.setEventListeners();
 profilePopup.setEventListeners();
 cardPopup.setEventListeners();
 
-editButton.addEventListener("click", () => {
-    profilePopup.setInputValues(userInfo.getUserInfo());
-    profilePopup.open();
-});
+editButton.addEventListener("click", handleEditButtonClick);
+addButton.addEventListener("click", handleAddButtonClick);
 
-addButton.addEventListener("click", () => {
-    cardPopup.open();
-});
-
-forms.forEach((form) => {
-    const formValidator = new FormValidator(validationSettings, form);
-    formValidator.enableValidation();
-});
-cardSection.renderItems();
+profileValidation.enableValidation();
+cardValidation.enableValidation();
+cardSection.renderItems(cards);
