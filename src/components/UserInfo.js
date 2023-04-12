1c464b3629b3f4607b1e13de62c7adda35e94690
Misha Kukarkin <wardenclock@gmail.com>
2023-04-12 21:42:28 +0500

    feature: added avatar support

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,17 +1,25 @@
 export default class UserInfo {
-    constructor({ nameSelector, aboutSelector }) {
+    constructor({ nameSelector, aboutSelector, pictureSelector }) {
         this._nameElement = document.querySelector(nameSelector);
         this._aboutElement = document.querySelector(aboutSelector);
+        this._pictureElement = document.querySelector(pictureSelector);
+        this._id = null;
     }
 
     getUserInfo() {
         const name = this._nameElement.textContent;
         const about = this._aboutElement.textContent;
-        return { name, about };
+        const id = this._id;
+        return { name, about, id };
     }
 
     setUserInfo(user) {
         this._nameElement.textContent = user.name;
         this._aboutElement.textContent = user.about;
+        this._id = user._id;
+        
+        if (user.avatar) {
+            this._pictureElement.src = user.avatar;
+        }
     }
 }
