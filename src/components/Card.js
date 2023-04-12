94c729247d25407583f492fdea781a70031740d1
Misha Kukarkin <wardenclock@gmail.com>
2023-04-12 21:40:12 +0500

    feature: refactored class so it works with API

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,43 +1,72 @@
 export default class Card {
-    constructor(cardData, templateSelector, handleCardClick) {
-        this._name = cardData.name;
-        this._link = cardData.link;
-        this._templateSelector = templateSelector;
-        this._handleCardClick = handleCardClick;
-    }
-  
-    _getTemplate() {
-        const cardTemplate = document.querySelector(this._templateSelector).content.querySelector('.elements__element').cloneNode(true);
-        return cardTemplate;
-    }
-  
-    _handleLikeClick() {
-        this._likeButton.classList.toggle('elements__like-button_active');
-    }
-  
-    _handleDeleteClick() {
-        this._element.remove();
-        this._element = null;
+  constructor(cardData, templateSelector, handleCardClick, handleLikeClick, handleDeleteClick) {
+    this._name = cardData.name;
+    this._link = cardData.link;
+    this._id = cardData._id;
+    this._ownerId = cardData.owner._id;
+    this._likes = cardData.likes;
+    this._templateSelector = templateSelector;
+    this._handleCardClick = handleCardClick;
+    this._handleLikeClick = handleLikeClick;
+    this._handleDeleteClick = handleDeleteClick;
+  }
+
+  _getTemplate() {
+    const cardTemplate = document.querySelector(this._templateSelector).content.querySelector('.elements__element').cloneNode(true);
+    return cardTemplate;
+  }
+
+  _handleToggle() {
+    this._likeButton.classList.toggle('elements__like-button_active');
+  }
+
+  _setEventListeners() {
+    this._cardImage.addEventListener('click', () => this._handleCardClick());
+
+    this._likeButton.addEventListener('click', () => {
+      this._handleLikeClick({
+        likeButton: this._likeButton,
+        id: this._id,
+        updateLikeCounter: this.updateLikeCounter.bind(this)
+      });
+      this._handleToggle();
+    }); 
+
+    this._deleteButton.addEventListener('click', () => {
+        this._handleDeleteClick({
+          cardElement: this._element,
+          id: this._id,
+          updateLikeCounter: this.updateLikeCounter.bind(this)
+        });
+    });
+  }
+
+  generateCard(canDelete, isLiked) {
+    this._element = this._getTemplate();
+    this._cardImage = this._element.querySelector('.elements__image');
+    this._likeButton = this._element.querySelector('.elements__like-button');
+    this._deleteButton = this._element.querySelector('.elements__delete-button');
+    this._likeCounter = this._element.querySelector('.elements__counter');
+
+    this._cardImage.src = this._link;
+    this._cardImage.alt = "Изображение: " + this._name;
+    this._element.querySelector('.elements__text').textContent = this._name;
+    this._element.querySelector('.elements__counter').textContent = this._likes ? this._likes.length : 0;
+
+    if (isLiked) {
+      this._handleToggle();
     }
 
-    _setEventListeners() {
-        this._cardImage.addEventListener('click', () => this._handleCardClick());
-        this._likeButton.addEventListener('click', () => this._handleLikeClick());
-        this._deleteButton.addEventListener('click', () => this._handleDeleteClick());
+    if (!canDelete) {
+      this._deleteButton.style.display = 'none';
     }
 
-    generateCard() {
-      this._element = this._getTemplate();
-      this._cardImage = this._element.querySelector('.elements__image');
-      this._likeButton = this._element.querySelector('.elements__like-button');
-      this._deleteButton = this._element.querySelector('.elements__delete-button');
-  
-      this._cardImage.src = this._link;
-      this._cardImage.alt = "Изображение: " + this._name;
-      this._element.querySelector('.elements__text').textContent = this._name;
-  
-      this._setEventListeners();
-  
-      return this._element;
-    }
+    this._setEventListeners();
+
+    return this._element;
+  }
+
+  updateLikeCounter(likes) {
+    this._element.querySelector('.elements__counter').textContent = likes;
+  }
 }
