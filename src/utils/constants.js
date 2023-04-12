6d5dcd85b147ea831bd180777c1f01e50d0c0b69
Misha Kukarkin <wardenclock@gmail.com>
2023-04-12 21:27:03 +0500

    updated constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,11 +2,19 @@ export const cardListSelector = ".elements__list";
 export const cardTemplateSelector = "#element-template";
 export const profileNameSelector = ".profile__title";
 export const profileAboutSelector = ".profile__subtitle";
+export const profilePictureSelector = ".profile__image";
 export const imagePopupSelector = "#popup__content_album";
 export const photoFormSelector = "#popup__content_photo"; 
 export const profileFormSelector = "#popup__content_profile"; 
+export const avatarFormSelector = "#popup__content_avatar";
+export const deleteFormSelector = "#popup__content_delete";
 export const addButtonSelector = ".profile__add-button";
 export const editButtonSelector = ".profile__edit-button"; 
+export const likeButtonActiveClass = "elements__like-button_active";
+export const overlaySelector = ".profile__overlay";
+
+export const token = "aaa4d623-3580-47cc-a27f-00cc795bd782";
+export const cohort = "cohort-64";
 
 export const validationSettings = {
   formSelector: '.popup__form',
@@ -16,30 +24,3 @@ export const validationSettings = {
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
 }
-
-export const cards = [
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    }
-];
